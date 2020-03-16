21f861555123f18b5eb7ee7a9090fdbb62d8cd26
Antonio Lorusso <mitictonylor@gmail.com>
2020-03-16 18:16:34 +0000

    fix indentation

--- code_clan_students/code_clan_students.rb
+++ code_clan_students/code_clan_students.rb
@@ -1,30 +1,31 @@
 class Student
-def initialize(name, cohort)
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
+
+  def get_name()
+    return @name
+  end
+
+  def get_cohort()
+    return @cohort
+  end
+
+  def set_name(name)
   @name = name
-  @cohort = cohort
-end
+  end
 
-def get_name()
-  return @name
-end
+  def set_cohort(name)
+    @cohort = name
+  end
 
-def get_cohort()
-  return @cohort
-end
+  def talk()
+    return "I can talk"
+  end
 
-def set_name(name)
-@name = name
-end
+  def say_favourite_language(language)
+    return "I love #{language}"
+  end
 
-def set_cohort(name)
-  @cohort = name
-end
-
-def talk()
-  return "I can talk"
-end
-
-def say_favourite_language(language)
-  return "I love #{language}"
-end
 end
--- code_clan_students/specs/code_clan_students_specs.rb
+++ code_clan_students/specs/code_clan_students_specs.rb
@@ -18,23 +18,23 @@ class TestStudent < Minitest::Test
     assert_equal("G18", @student.get_cohort())
   end
 
-def test_set_student_name()
-  @student.set_name("Gino")
-  assert_equal("Gino", @student.get_name())
-end
+  def test_set_student_name()
+    @student.set_name("Gino")
+    assert_equal("Gino", @student.get_name())
+  end
 
-def test_set_student_cohort()
-  @student.set_cohort("G19")
-  assert_equal("G19", @student.get_cohort())
-end
+  def test_set_student_cohort()
+    @student.set_cohort("G19")
+    assert_equal("G19", @student.get_cohort())
+  end
 
-def test_talk()
-  assert_equal("I can talk", @student.talk())
-end
+  def test_talk()
+    assert_equal("I can talk", @student.talk())
+  end
 
-def test_say_favourite_language()
-  assert_equal("I love Ruby", @student.say_favourite_language("Ruby"))
-end
+  def test_say_favourite_language()
+    assert_equal("I love Ruby", @student.say_favourite_language("Ruby"))
+  end
 
 
 end
