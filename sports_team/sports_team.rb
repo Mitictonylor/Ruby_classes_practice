4a01f3c128572b744fcfd2a11c3661954c108ba9
Antonio Lorusso <mitictonylor@gmail.com>
2020-03-16 19:03:15 +0000

    add get_team_players, test pass

--- sports_team/sports_team.rb
+++ sports_team/sports_team.rb
@@ -1,13 +1,19 @@
 class SportTeam
-  def initialize(name,player,coach)
+  def initialize(name,players,coach)
     @name = name
-    @player = player
+    @player = players
     @coach = coach
   end
 
 def get_team_name()
   return @name
 end
+
+def get_team_players()
+  return @player
+end
+
+
 
 
 end
